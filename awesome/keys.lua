fd82dcf2d7a5e14c3911c625c406e33e7811d3cb
Markus Fuchs <web-code@mfuchs.org>
2014-05-27 10:58:25 +0200

    fixed window restore functionality

--- awesome/keys.lua
+++ awesome/keys.lua
@@ -92,7 +92,7 @@ globalkeys = awful.util.table.join(
     awful.key({ modkey,           }, "space", function () awful.layout.inc(layouts,  1) end),
     awful.key({ modkey, "Shift"   }, "space", function () awful.layout.inc(layouts, -1) end),
 
-    awful.key({ modkey, "Control" }, "n", awful.client.restore()),
+    awful.key({ modkey, "Control" }, "n", function () awful.client.restore() end),
     -- awful.key({ modkey }, "q", function () mousefinder:find() end),
 
     -- Prompt
